9395798ff2e2cb6bcac3f0b986c89abf33d9dee5
tomsi365 <tomsi365@gmail.com>
2021-07-09 01:52:53 +0300

    fix

--- src/Simulator.js
+++ src/Simulator.js
@@ -55,7 +55,7 @@ function getResults(vertexes,setResult){
         table.push(row)
     }
 
-    fetch(' https://cors-everywhere.herokuapp.com/http://dijkstrabackenv-env.eba-ikrw47ps.us-east-2.elasticbeanstalk.com/',
+    fetch(' https://cors-everywhere.herokuapp.com/http://dijkstrabackenv-env-1.eba-ikrw47ps.us-east-2.elasticbeanstalk.com/',
         {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
