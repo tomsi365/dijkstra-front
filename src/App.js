f868268a1b2906865f1bb54604817e62cfe42f6e
tomsi365 <tomsi365@gmail.com>
2021-07-08 23:29:04 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Simulator from "./Simulator";
+
+function Home(){
+    return (
+        <div className='Content'>
+            Welcome to my personal Dijkstra's Algorithm project .
+            <br/>
+            As a part of my academic studies ,
+            <br/>
+            I was asked to build a project about Dijkstra's Algorithm .
+        </div>
+    );
+}
+
+function Algorithm(){
+    return(
+        <div className='Content'>
+            Dijkstra(Graph, source): <br/>
+            <br/>
+            &nbsp;&nbsp;d[source] = 0 <br/>
+            &nbsp;&nbsp;create vertex priority queue Q <br/>
+            <br/>
+            &nbsp;&nbsp;for each vertex v in Graph: <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;if v != source <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d[v] = INFINITY <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;p[v] = UNDEFINED <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Q.add(v, d[v]) <br/>
+            <br/>
+            &nbsp;&nbsp;while Q is not empty: <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;u = Q.deleteMin() <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;for each neighbor v of u: <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;alt = d[u] + length(u, v) <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if alt &#60; d[v] <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d[v] = alt <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;p[v] = u <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Q.decreaseKey(v, alt) <br/>
+            <br/>
+            &nbsp;&nbsp;return d , p <br/>
+            <br/>
+            ** assumed that graph's weights are positive <br/>
+        </div>
+    );
+}
+
+function NavigationBar(props){
+    let setContent=props.setContent
+    return(
+        <ul className="Navigation-Bar">
+            <li><button onClick={()=>setContent(0)}>Home</button></li>
+            <li><button onClick={()=>setContent(1)}>Algorithm</button></li>
+            <li><button onClick={()=>setContent(2)}>Simulator</button></li>
+        </ul>
+    );
+}
+
+function Content(props){
+    let content=props.content
+    if(content===0)
+        return Home()
+    else if(content===1)
+        return Algorithm()
+    else
+        return Simulator(props.simulator,props.setSimulator,props.fetch,props.setFetch,props.result,props.setResult)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [content,setContent] = useState(0);
+    const [simulator,setSimulator] = useState(0);
+    const [fetch,setFetch] = useState(0);
+    const [result,setResult] = useState(0);
+    return (
+        <div className="App">
+            <NavigationBar setContent={setContent}/>
+            <Content
+                fetch={fetch} setFetch={setFetch}
+                simulator={simulator} setSimulator={setSimulator}
+                result={result} setResult={setResult}
+                content={content}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Simulator.js
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+
+function Simulator(simulator,setSimulator,fetch,setFetch,result,setResult){
+    if(simulator===0)
+        return Vertexes(setSimulator,setFetch)
+    else if (simulator>0&&fetch===0)
+        return Weights(simulator,setSimulator,setFetch,setResult)
+    else
+        return Results(setFetch,result);
+}
+
+function WeightsTable(vertexes){
+    let table=[]
+    table.push(<tr>{WeightsHeaderRow(vertexes)}</tr>)
+    let i
+    for(i=0;i<vertexes;i++){
+        table.push(<tr>{WeightsRow(vertexes,i)}</tr>)
+    }
+    return table
+}
+
+function WeightsHeaderRow(vertexes){
+    let row=[]
+    row.push(<td></td>)
+    let j
+    for(j=0;j<vertexes;j++){
+        row.push(<th>{j}</th>)
+    }
+    return row;
+}
+
+function WeightsRow(vertexes,i){
+    let row=[]
+    row.push(<th>{i}</th>)
+    let j
+    for(j=0;j<vertexes;j++){
+        let id='id'+i+'_'+j
+        row.push(<td><input type="number" id={id} name={id} min="0" max="10" defaultValue="0" /></td>)
+    }
+    return row;
+}
+
+function getResults(vertexes,setResult){
+
+    let source=document.querySelector('#source').value
+    let table=[]
+    let i,j
+    for(i=0;i<vertexes;i++){
+        let row= []
+        for(j=0;j<vertexes;j++){
+            let query='#id'+i+'_'+j
+
+            row.push(document.querySelector(query).value)
+        }
+        table.push(row)
+    }
+
+    fetch('http://dijkstrabackenv-env.eba-ikrw47ps.us-east-2.elasticbeanstalk.com/',
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                table:table,
+                source:source
+            })
+        }
+    )
+        .then((response) => {return response.json()})
+        .then((responseData) => {return responseData})
+        .then((data) => {setResult(data)})
+        .catch((err) => {console.log(err)})
+}
+
+function DRow(result){
+    let row=[]
+
+    row.push(<th>d</th>);
+    let i
+    for(i=0;i<result.d.length-1;i++){
+        row.push(<td>{result.d[i]}</td>);
+    }
+    row.push(<td>{result.d[i]}</td>);
+    return row
+}
+
+function PRow(result){
+    let row=[]
+
+    row.push(<th>p</th>);
+    let i
+    for(i=0;i<result.p.length-1;i++){
+        row.push(<td>{result.p[i]}</td>);
+    }
+    row.push(<td>{result.p[i]}</td>);
+    return row
+}
+
+function ResultsHeaderRow(result){
+    let row=[]
+
+    row.push(<th>vertex</th>);
+    let i
+    for(i=0;i<result.p.length;i++){
+        row.push(<th>{i}</th>);
+    }
+    return row
+}
+
+function ResultsTable(result){
+    let table=[]
+    if(result===0){
+        table.push('fetching...')
+    }
+    else{
+        table.push(<tr>{ResultsHeaderRow(result)}</tr>)
+        table.push(<tr>{DRow(result)}</tr>)
+        table.push(<tr>{PRow(result)}</tr>)
+    }
+    return table
+}
+
+function Weights(simulator,setSimulator,setFetch,setResult){
+    let i
+    return(
+        <div className='Content'>
+            Please choose graph's weights (case there isn't edge -> choose 0).
+            <br/>
+            <br/>
+            <table>{WeightsTable(simulator)}</table>
+            <br/>
+            Source :
+            <br/>
+            <input type="number" id="source" name="source" min="0" max={simulator-1} defaultValue="0" />
+            <br/>
+            <br/>
+            <button onClick={()=>setSimulator(0)}>back</button>
+            <br/>
+            <button onClick={()=>{
+                getResults(simulator,setResult)
+                setFetch(1)
+            }}>run</button>
+        </div>
+    );
+}
+
+function Results(setFetch,result){
+    return(
+        <div className='Content'>
+            Results :
+            <br/>
+            <br/>
+            <table>{ResultsTable(result)}</table>
+            <br/>
+            <button onClick={()=>setFetch(0)}>back</button>
+        </div>
+    );
+}
+
+function Vertexes(setSimulator){
+    return(
+        <div className='Content'>
+            Please choose how much vertexes you want . <br/>
+            <br/>
+            <input type="number" id="vertexes" name="vertexes" min="2" max="9" defaultValue="2" />
+            <br/>
+            <br/>
+            <button onClick={()=>setSimulator(document.querySelector('#vertexes').value)}>next</button>
+        </div>
+    );
+}
+
+export default Simulator;
